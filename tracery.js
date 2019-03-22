b9ca781bbc454cd42c893112613715b21914b33a
George Buckenham <vtwentyone@gmail.com>
2019-03-22 14:14:04 +0000

    remove console warnings

--- tracery.js
+++ tracery.js
@@ -32,7 +32,7 @@ var tracery = function() {
         this.isExpanded = false;
 
         if (!this.grammar) {
-            console.warn("No grammar specified for this node", this);
+            this.errors.push("No grammar specified for this node " + this);
         }
 
     };
@@ -70,7 +70,6 @@ var tracery = function() {
         } else {
             // In normal operation, this shouldn't ever happen
             this.errors.push("No child rule provided, can't expand children");
-            console.warn("No child rule provided, can't expand children");
         }
     };
 
